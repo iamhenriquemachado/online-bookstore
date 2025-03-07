f5e772a952670aea6380b0279f846529df5cdd0c
Henrique Machado <heyhenriquecastro@gmail.com>
2025-03-07 12:15:23 -0300

    Routes implementation

--- routes/books.py
+++ routes/books.py
@@ -1,23 +1,34 @@
-from models.Book import BookModel
 from fastapi import APIRouter, HTTPException
+from models.Book import Book
+import sqlite3
+from typing import List, Dict
+
+connection = sqlite3.connect('database/onlinestore.db')
+connection.row_factory = sqlite3.Row
+cursor = connection.cursor()
 
 router = APIRouter()
 
 # Route URL: http://127.0.0.1:8000/books/post
 @router.post('/post')
-async def new_book(bookdata: BookModel):
-    bookdata.id = BookModel.generate_id()
+async def new_book(bookdata: Book):
+    bookdata.id = Book.generate_id()
 
     return {
         "message": "New book created!", 
         "bookInformation": bookdata.model_dump()
     }
 
-@router.post('/getbooks')
+@router.get('/all', response_model=List[Dict])
 async def get_all_books():
     try:
-        return {
-            "bookInformation": bookdata.model_dump()
-            }
+        query = '''SELECT * FROM Books'''
+        cursor.execute(query)
+
+        books = [dict(row) for row in cursor.fetchall()]
+        connection.close()
+
+        return books
+    
     except:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="Item not found")  
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-@app.get("/")
-def server_running():
-    return {"Message": "Server is running ✨"}
